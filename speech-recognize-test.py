a46219ae1bf14f240fb324d0b38864c341fff25e
Mustafa Siam Ur Rafique <siam11651@outlook.com>
2022-08-04 11:32:48 +0600

    Update speech-recognize-test.py

--- speech-recognize-test.py
+++ speech-recognize-test.py
@@ -1,15 +1,24 @@
 import speech_recognition
+import pyaudio
 
 # ami bujhi...
 
 recognizer = speech_recognition.Recognizer()
+recognizer.energy_threshold = 1000
 
 with speech_recognition.Microphone() as source:
+    print('calibrating mic...')
+    recognizer.adjust_for_ambient_noise(source)
+    print('record shuru holo...')
+
     audio = recognizer.listen(source)
+
+print('record shesh')
 
 # source hoilo amar mic re python variable e nilam
 # recognizer object er listen method voice recognize
 # kore dibe
+# pause threshold function 1 second silence pele record off korbe
 
 try:
     print(recognizer.recognize_google(audio))
